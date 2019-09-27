5141a3a0298ec588b82a8a1052ea8d271e73df21
gregoryCommTV <55813081+gregoryCommTV@users.noreply.github.com>
2019-09-27 17:29:16 -0400

    Cleaned up, bug fixes and created start up script

---
+++ start.py
@@ -0,0 +1,18 @@
+#/usr/bin/env python
+import subprocess
+
+cmd = ['lcp', '--proxyUrl', 'https://condocontrolcentral.com']
+out = subprocess.Popen(cmd, cwd="./", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print(cmd, out)
+
+cmd = ['lcp', '--proxyUrl', 'https://citymapper.com', '--port', '8011']
+out = subprocess.Popen(cmd, cwd="./", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print(cmd, out)
+
+cmd = ['lcp', '--proxyUrl', 'https://api.darksky.net', '--port', '8012']
+out = subprocess.Popen(cmd, cwd="./", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print(cmd, out)
+
+cmd = ['python', '-m', 'http.server'] 
+out = subprocess.Popen(cmd, cwd="./", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print(cmd, out)
