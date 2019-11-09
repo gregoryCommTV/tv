b5f2dbab19b3baef01d4203af095b5d1b70b5edf
gregoryCommTV <55813081+gregoryCommTV@users.noreply.github.com>
2019-11-09 18:53:03 -0500

    Add files via upload

---
+++ TV.command
@@ -0,0 +1,5 @@
+#!/bin/bash
+pushd /Users/gc/devel/tv/
+./start.sh
+popd
+exit
--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-lcp --proxyUrl https://condocontrolcentral.com &
+kill $(ps aux | grep '[c]ondocontrol' | awk '{print $2}')
+nohup lcp --proxyUrl https://condocontrolcentral.com &
 
-lcp --proxyUrl https://citymapper.com --port 8011 &
+kill $(ps aux | grep '[c]itymapper' | awk '{print $2}')
+nohup lcp --proxyUrl https://citymapper.com --port 8011 &
 
-lcp --proxyUrl https://api.darksky.net --port 8012 &
+kill $(ps aux | grep '[d]arksky' | awk '{print $2}')
+nohup lcp --proxyUrl https://api.darksky.net --port 8012 &
 
-python -m http.server
+kill $(ps aux | grep '[h]ttp.server' | awk '{print $2}')
+nohup python3 -m http.server &
 
-../../Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe -start-fullscreen http://127.0.0.1:8000
+open -a "Google Chrome" --new --args -kiosk http://127.0.0.1:8000
