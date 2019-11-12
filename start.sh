8392282cebabe87b58c2842f26a2889c92b16907
gregoryCommTV <55813081+gregoryCommTV@users.noreply.github.com>
2019-11-12 16:29:04 -0500

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-kill $(ps aux | grep '[c]ondocontrol' | awk '{print $2}')
-nohup lcp --proxyUrl https://condocontrolcentral.com &
-
-kill $(ps aux | grep '[c]itymapper' | awk '{print $2}')
-nohup lcp --proxyUrl https://citymapper.com --port 8011 &
-
-kill $(ps aux | grep '[d]arksky' | awk '{print $2}')
-nohup lcp --proxyUrl https://api.darksky.net --port 8012 &
+kill $(ps aux | grep '[c]orsProxy' | awk '{print $2}')
+nohup python3 corsProxy.py &
 
 kill $(ps aux | grep '[h]ttp.server' | awk '{print $2}')
 nohup python3 -m http.server &
