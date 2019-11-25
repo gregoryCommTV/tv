c292e8e97f0760f1def0015564050d4c20797846
KOUNEV <vk0366@attadmins-MBP-2.lan>
2019-11-25 15:05:57 -0500

    Added code to prevent browser cashing

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#build image
-docker build -t gregory_commons_tv .
-
-#stop docker image is running
-docker stop $(docker ps -q --filter ancestor=gregory_commons_tv )
-
-#run image
-docker run -p 9000:8000 gregory_commons_tv
